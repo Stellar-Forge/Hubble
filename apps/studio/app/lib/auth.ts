6a710b2313dfe6d2bb8dfe9e1139764df5a02d71
rahulbarmann <itsrahulbarman1@gmail.com>
2024-07-28 00:32:07 +0530

    Codebase Initialization

---
+++ apps/studio/app/lib/auth.ts
@@ -0,0 +1,61 @@
+import prisma from "@repo/prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+    providers: [
+      CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            username: { label: "Username", type: "text", placeholder: "Username" },
+            password: { label: "Password", type: "password", placeholder: "Password" }
+          },
+          // TODO: User credentials type from next-aut
+          async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            const existingUser = await prisma.user.findFirst({
+                where: {
+                    username: credentials.username
+                }
+            });
+
+            if (existingUser) {
+                console.log("Existing user")
+                console.log(credentials.password)
+                console.log(existingUser.password)
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    console.log("Pass valid")
+
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.username,
+                        email: existingUser.email
+                    }
+                }
+                console.log("Pass Invalid")
+
+                return null;
+            }
+            return null
+          },
+        }),
+        
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+          })
+        
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        // TODO: fix the type here
+        async session({ token, session }: any) {
+            session.user.id = token.sub
+
+            return session
+        }
+    }
+  }
+ 
---
+++ packages/ui/src/Appbar.tsx
@@ -0,0 +1,30 @@
+import { Button } from "./button";
+
+interface AppbarProps {
+    user?: {
+        name?: string | null;
+    },
+    // TODO: what type should be here?
+    onSignin: any,
+    onSignout: any
+}
+
+export const Appbar = ({
+    user,
+    onSignin,
+    onSignout
+}: AppbarProps) => {
+    return <div className="flex justify-between border-b px-4 w-full">
+        <div className="text-lg flex flex-col justify-center">
+            Hubble
+        </div>
+        <div className="flex items-center space-x-10">
+            <div>
+                {user?.name}
+            </div>
+            <div className="flex flex-col justify-center pt-2">
+                <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
+            </div>
+        </div>
+    </div>
+}
