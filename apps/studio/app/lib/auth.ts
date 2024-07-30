ed931cb39ec65b8bc4e6e56d7912fc07e78b1913
rahulbarmann <itsrahulbarman1@gmail.com>
2024-07-30 06:45:16 +0530

    feat: Added Login with Github

--- apps/studio/app/lib/auth.ts
+++ apps/studio/app/lib/auth.ts
@@ -1,6 +1,7 @@
 import prisma from "@repo/prisma/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import bcrypt from "bcrypt";
 import { signinSchema } from "../../../../packages/zod-schema/userAuthSchema";
 
@@ -65,6 +66,11 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+          }),
+
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || ""
           })
         
     ],
