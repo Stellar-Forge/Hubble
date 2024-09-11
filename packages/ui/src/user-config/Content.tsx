653f891181c88b16abcd8455fd5b700bce6fb9e4
rahulbarmann <itsrahulbarman1@gmail.com>
2024-09-11 18:28:45 +0530

    refactor: integrate all features of user-config from next-dev

--- packages/ui/src/user-config/Content.tsx
+++ packages/ui/src/user-config/Content.tsx
@@ -59,13 +59,15 @@ export function Content() {
     const platform = mapPlatform(model);
 
     return (
-        <div>
-            <div>Your API Keys Linked With This Account:</div>
-            <div>{JSON.stringify(userKeys.map((e: any) => e.platform))}</div>
-            <label>
-                Add Your API Key for Model:
+        <div className="flex flex-col grow justify-center items-center">
+            <label className="m-3">
+                <h1 className="text-6xl font-bold drop-shadow-[2px_2px_1px_rgba(0,0,0,0.45)] transition hover:drop-shadow-[10px_10px_5px_rgba(0,0,0,0.45)] ease-out duration-500">
+                    Add Your API Keys
+                </h1>
+            </label>
+            <div className="m-5 w-full flex justify-center items-center">
                 <select
-                    className="m-7"
+                    className="border-2 border-black p-2 rounded-lg"
                     value={model}
                     onChange={(e) => {
                         setModel(e.target.value);
@@ -75,79 +77,78 @@ export function Content() {
                     <option value="Gemini">Gemini</option>
                     <option value="GetImgAI">GetImg.AI</option>
                 </select>
-            </label>{" "}
-            <br />
-            <input
-                className="m-7"
-                value={input}
-                type="text"
-                placeholder="API KEY"
-                onChange={(e) => {
-                    setInput(e.target.value);
-                    setSaveButtonVisible(false);
-                }}
-            ></input>
-            <button
-                className="m-7"
-                onClick={async () => {
-                    const isEmpty = (value: string) =>
-                        value.trim().length === 0;
-                    if (isEmpty(input)) alert("No API Key Entered!");
-                    else {
-                        setLoading(true);
-                        const res = await checkAPIKey(input);
-                        setLoading(false);
-                        if (!res.success) {
-                            alert("Invalid API Key!");
-                            setSaveButtonVisible(false);
-                        } else if (model === "Gemini") {
-                            setUsableModels(res.response.models);
-                            setSaveButtonVisible(true);
-                        } else {
-                            setUsableModels(res.response);
-                            setSaveButtonVisible(true);
-                        }
-                    }
-                }}
-            >
-                Check
-            </button>
-            {saveButtonVisible ? (
+                <input
+                    className="m-3 border-2 border-black p-2 rounded-lg w-1/2"
+                    value={input}
+                    type="text"
+                    placeholder="API KEY"
+                    onChange={(e) => {
+                        setInput(e.target.value);
+                        setSaveButtonVisible(false);
+                    }}
+                />
                 <button
-                    className="m-7"
+                    className="border-2 border-black p-2 rounded-lg"
                     onClick={async () => {
-                        const isAlreadySaved = userKeys.some(
-                            (e: any) => e.platform === platform,
-                        );
-                        if (isAlreadySaved)
-                            alert(
-                                `You Have Already Saved An API Key For ${model}, Delete It First To Save A New API Key!`,
-                            );
+                        const isEmpty = (value: string) =>
+                            value.trim().length === 0;
+                        if (isEmpty(input)) alert("No API Key Entered!");
                         else {
-                            const encryptedCode = encryptApiKey(input);
                             setLoading(true);
-                            type ModelPlatform = "Gemini" | "GetImgAI";
-                            const modelPlatform = model as ModelPlatform; // telling model is type ModelPlatform
-                            const res = await saveAPIKey(
-                                encryptedCode,
-                                modelPlatform,
-                            );
+                            const res = await checkAPIKey(input);
                             setLoading(false);
-                            if (!res) alert("Some Error Occured!");
-                            else {
-                                setKeyDidUpdate(true);
-                                alert("API Key Saved Successfully!");
+                            if (!res.success) {
+                                alert("Invalid API Key!");
+                                setSaveButtonVisible(false);
+                            } else if (model === "Gemini") {
+                                setUsableModels(res.response.models);
+                                setSaveButtonVisible(true);
+                            } else {
+                                setUsableModels(res.response);
+                                setSaveButtonVisible(true);
                             }
                         }
                     }}
                 >
-                    Save
+                    Check
                 </button>
-            ) : (
-                ""
-            )}
-            <br />
-            <br />
+                {saveButtonVisible ? (
+                    <button
+                        className="ml-3 border-2 border-black p-2 rounded-lg"
+                        onClick={async () => {
+                            const isAlreadySaved = userKeys.some(
+                                (e: any) => e.platform === platform,
+                            );
+                            if (isAlreadySaved)
+                                alert(
+                                    `You Have Already Saved An API Key For ${model}, Delete It First To Save A New API Key!`,
+                                );
+                            else {
+                                const encryptedCode = encryptApiKey(input);
+                                setLoading(true);
+                                type ModelPlatform = "Gemini" | "GetImgAI";
+                                const modelPlatform = model as ModelPlatform; // telling model is type ModelPlatform
+                                const res = await saveAPIKey(
+                                    encryptedCode,
+                                    modelPlatform,
+                                );
+                                setLoading(false);
+                                if (!res) alert("Some Error Occured!");
+                                else {
+                                    setKeyDidUpdate(true);
+                                    alert("API Key Saved Successfully!");
+                                }
+                            }
+                        }}
+                    >
+                        Save
+                    </button>
+                ) : (
+                    ""
+                )}
+            </div>
+            <div>Connected API Keys - </div>
+            <div>{JSON.stringify(userKeys.map((e: any) => e.platform))}</div>
             {`Your Available Models For Current API Key: `}
             {saveButtonVisible
                 ? usableModels.map((e: any, index) => (
@@ -155,9 +156,6 @@ export function Content() {
                           key={index}
                           displayName={
                               model === "Gemini" ? e.displayName : e.name
-                          }
-                          description={
-                              model === "Gemini" ? e.description : e.family
                           }
                       />
                   ))
